68494163ba8daf590542ded543de5ec866a64a2d
Abdullah Sanver <a_sanver@hotmail.com>
2020-05-01 19:56:30 +0100

    PT-002 Bismillahirrahmanirrahim. Adds project for learning html

---
+++ src/main/java/uk/co/sancode/learning_html/LearningHtmlApplication.java
@@ -0,0 +1,13 @@
+package uk.co.sancode.learning_html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningHtmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/sancode/learning_html/LearningHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.sancode.learning_html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningHtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
